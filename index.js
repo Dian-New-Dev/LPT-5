88a8b2666b9885948bbf9b852038ddd2e6951bd4
Diego <diannewdev@gmail.com>
2024-06-12 20:39:08 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,65 @@
+const header = {
+    logo: 'images/logo.png',
+    a: ['Inicio', 'Sobre mim', 'Especialidades', 'FAQ', 'Testemunhos']
+
+}
+
+const hero = {
+    img: 'images/hero.png',
+    h1: 'Psicoterapia em Campo Grande',
+    p: 'Procurar Ajuda é um Ato de Coragem. Lhe auxilio nesse processo, oferecendo sessões presenciais ou remotas, feitas sob medida para a sua demanda.',
+    button: 'Agendar uma Consulta',
+}
+
+const sobre = {
+    img: 'images/foto.png',
+    h2: 'Sobre Mim',
+    p1: 'Olá! Meu nome é Jéssica Silva, sou psicóloga especializada em terapia cognitivo-comportamental (TCC) e possuo mais de 10 anos de experiência ajudando pessoas a superar desafios emocionais e psicológicos. Minha missão é proporcionar um espaço seguro e acolhedor onde meus pacientes possam explorar suas emoções, desenvolver habilidades de enfrentamento e encontrar um caminho para uma vida mais equilibrada e satisfatória.',
+    p2: 'Com uma abordagem personalizada, busco entender as necessidades individuais de cada pessoa, oferecendo suporte contínuo e estratégias eficazes para promover mudanças positivas. Seja para lidar com ansiedade, depressão, estresse ou outros problemas emocionais, estou aqui para ajudar você a alcançar seu bem-estar e crescimento pessoal.',
+    icon: 'images/icons/quotes.png',
+    h4: ['Minhas Credenciais', 'Minha Formação', 'Meus Certificados'],
+    seta: 'images/icons/seta.png',
+    resposta1: ['Membro da Sociedade Brasileira de Psicologia', 'Fundadora da ONG Prezar Universal', 'Professora Visitante na Universidade Federal de São Carlos'],
+    resposta2: ['Graduação em Psicologia pela Universidade de São Paulo (USP)', 'Mestrado em Psicologia Clínica com especialização em Terapia Cognitivo-Comportamental pela Pontifícia Universidade Católica de São Paulo (PUC-SP)', 'Certificação Internacional em Terapia Cognitivo-Comportamental (CBT) pelo Beck Institute for Cognitive Behavior Therapy'],
+    resposta2: ['Prêmio Excelência em Psicoterapia 2021', 'Honra ao Mérito em Saúde Mental 2019']
+}
+
+const especialidades = {
+    h2: 'Minhas Especialidades',
+    img: ['images/icons/esp1.png', 'images/icons/esp1.png', 'images/icons/esp1.png', 'images/icons/esp1.png'],
+    h5: ['Ansiedade e Depressão', 'Casais e Relacionamento', 'Estresse Pós-Traumático', 'Estresse e Burnout'],
+
+}
+
+const testemunhos = {
+    h2: 'Testemunhos',
+    h4: 'Veja o que pessoas a quem ajudei ou ajudo têm a dizer sobre nossas sessões.',
+    img: 'images/icons/quotes.png',
+    h5: ['Maria', 'Tiago', 'Jorge'],
+    p: ['"A Dra. Mariana Silva mudou minha vida. Estava lutando contra a ansiedade há anos e nunca havia encontrado um terapeuta que realmente me entendesse. Com sua abordagem acolhedora e técnicas eficazes, consegui controlar minha ansiedade e agora me sinto muito mais confiante e feliz."',
+        '"Procuramos a Dra. Mariana para terapia de casal e não poderíamos estar mais satisfeitos. Ela nos ajudou a melhorar nossa comunicação e a resolver conflitos que estavam nos afetando há muito tempo. Hoje, nosso relacionamento está mais forte do que nunca, graças ao seu suporte e orientação."',
+        '"Após um evento traumático, estava tendo dificuldades para seguir em frente. A Dra. Mariana foi uma luz no fim do túnel. Sua paciência e competência em lidar com meu TEPT fizeram toda a diferença. Agora, sinto que posso finalmente viver minha vida plenamente de novo."'
+    ]
+}
+
+const FAQ = {
+    h2: 'Dúvidas Frequentes',
+    h3: [
+        'Quais são as modalidades de atendimento oferecidas?',
+        'Como funciona a primeira sessão de terapia?',
+        'Quanto tempo dura cada sessão de terapia?',
+        'A terapia é confidencial?'
+    ],
+    p: [
+        'Ofereço sessões de terapia presencial em meu consultório e também atendimentos online via videoconferência, permitindo maior flexibilidade para os pacientes que têm dificuldades de deslocamento ou preferem a comodidade de serem atendidos em casa.',
+        'Na primeira sessão, realizamos uma avaliação inicial para entender suas necessidades e objetivos. Discutimos seu histórico emocional e psicológico, e juntos elaboramos um plano de tratamento personalizado para ajudá-lo a alcançar seus objetivos de bem-estar.',
+        'Cada sessão de terapia dura aproximadamente 50 minutos. A frequência e a duração do tratamento variam conforme as necessidades individuais de cada paciente, podendo ser ajustadas ao longo do processo terapêutico.',
+        'Sim, todas as sessões de terapia são completamente confidenciais. Asseguro a privacidade e a confidencialidade de todas as informações compartilhadas, seguindo as diretrizes éticas e profissionais estabelecidas pelo Conselho Federal de Psicologia.',
+    ]
+
+}
+
+const inspiracao = {
+    citacao: '"A paz vem de dentro de você mesmo. Não a procure à sua volta."',
+    autor: 'Buda',
+}
